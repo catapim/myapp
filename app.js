79453a675702687ebe6a37a17fc1230ee5241377
cartulina <catalinapimentel@gmail.com>
2019-08-24 22:22:11 -0400

    there is getInfo, that function allows me to check information every 1 second, getInfo function has inside an ajax call that allows me to get the population number from the server, every second it increments

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var population = 0
+
+// game tick
+setInterval(function() {
+    population=population+1;
+},1000)
+
 app.use(express.static('dist'))
 
 // app.get('/', (req, res) => res.send(''))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/info', (req, res) => res.json({"population":population}));
+
--- src/MiniCiudad.jsx
+++ src/MiniCiudad.jsx
@@ -7,23 +7,43 @@ class MiniCiudad extends Component {
         this.state = {
             "population":0,
             "residential_zones":0
-        }        
+
+        }       
+        setInterval(()=>{
+            this.getInfo();   
+        },1000)
     }
 
+    componentDidMount() {
+        
+    }
+    getInfo() {
+        console.log("[get info]")
+        fetch('/info').then(res => res.json()).then(
+            (result) => {
+                console.log(result)
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+
+    }
     addResidentialZone(){
         alert("add residential zone")
     }
 
     render() {
-        return (
+        return(
+        
             <div>
+                
                 <h1>miniciudad</h1>
                 <p>population: {this.state.population}</p>
-                <p>residential zones: {this.state.residential_zones}</p>
-                
+                <p>residential zones: {this.state.residential_zones}</p>                
                 <button onClick={this.addResidentialZone}>add residential zone</button>
             </div>           
-        );
+)
     }
 }
 
